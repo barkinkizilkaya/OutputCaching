f2bc00e208e5674d76ee41f2a9d396848654b075
barkinkizilkaya <barkin.kizilkaya@outlook.com>
2022-11-22 19:23:13 +0300

    initial commit

---
+++ OutputCaching/Controllers/CacheController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace OutputCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [OutputCache(PolicyName = "ByQuery")]
+    public class CacheController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string Culture)
+        {
+            return Ok(DateTime.Now.ToString());
+        }
+
+       
+    }
+}
---
+++ OutputCaching/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(builder =>
+    {
+        builder.Expire(TimeSpan.FromSeconds(30));
+    });
+    options.AddPolicy("ByQuery",policy =>
+    {
+        policy.Expire(TimeSpan.FromSeconds(30));
+        policy.SetVaryByQuery("Culture");
+    });
+});
+var app = builder.Build();
+app.UseOutputCache();
+
+app.MapGet("/",[OutputCache( Duration =5)] () =>
+{
+    return Results.Ok(DateTime.Now);
+}).CacheOutput();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
